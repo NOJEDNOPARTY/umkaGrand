3a993781e2e7d032a008733dd0abfbc4f1fb4484
NOJEDNOPARTY <bordyuh@gmail.com>
2020-04-08 01:12:44 +0300

    add few boxes

--- js/app.js
+++ js/app.js
@@ -16,6 +16,27 @@ var common = {
 		
 		// var bLazy = new Blazy({});
 
+		$('.anchor').click(function(event){
+			event.preventDefault();
+			var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 2000);
+			$('header').removeClass('open');
+		});
+
+		$('.bnf-trigger').click(function(event){
+			event.preventDefault();
+			$('.popup .popup-info-wrapper').find('.popup-info').remove();
+			$(this).closest('.bnf-cnt').find('.popup-info').clone().appendTo( '.popup .popup-info-wrapper');
+			$('#benefitsPopup').addClass('active');
+		});
+
+		$('.popup-close').click(function(event){
+			event.preventDefault();
+			$('.popup-wrapper').removeClass('active');
+		});
+
+
 
 	},
 	owl: function(){
